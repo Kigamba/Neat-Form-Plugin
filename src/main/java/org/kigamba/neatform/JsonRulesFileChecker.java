7d6c77647c75efc5e073bff7403415051f4e448a
Ephraim Kigamba <n.kigamba@students.ku.ac.ke>
2020-05-08 23:21:43 +0300

    Save the field names in neat form files
    - This will enable us to check for wrong references
    - This will enable us to provide auto-complete for field references

--- src/main/java/org/kigamba/neatform/JsonRulesFileChecker.java
+++ src/main/java/org/kigamba/neatform/JsonRulesFileChecker.java
@@ -2,47 +2,72 @@ package org.kigamba.neatform;
 
 
 import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.json.psi.JsonArray;
+import com.intellij.json.psi.JsonProperty;
 import com.intellij.json.psi.JsonStringLiteral;
-import com.intellij.json.psi.JsonValue;
 import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLiteralExpression;
 import org.jetbrains.annotations.NotNull;
-import org.kigamba.simple.SimpleSyntaxHighlighter;
 
 import java.io.File;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class JsonRulesFileChecker implements Annotator {
+
+    private HashMap<String, HashSet<String>> filesFieldNames = new HashMap<>();
 
     @Override
     public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
         // Ensure the Psi Element is an expression
         if (!(element instanceof JsonStringLiteral)) return;
 
-        JsonStringLiteral literalExpression = (JsonStringLiteral) element;
-        String rulesFilePath = literalExpression.getValue();
+        JsonStringLiteral jsonStringLiteral = (JsonStringLiteral) element;
+        String rulesFilePath = jsonStringLiteral.getValue();
+
+        boolean isPropertyValue = isPropertyValue(jsonStringLiteral);
+        String filePath = element.getContainingFile().getVirtualFile().getPath();
 
         // Check if the rules file exists
-        if (literalExpression.getParent() != null) {
-            PsiElement propertyName = literalExpression.getParent().getFirstChild();
-            if (propertyName != literalExpression && propertyName instanceof JsonStringLiteral && ((JsonStringLiteral) propertyName).isPropertyName() && ((JsonStringLiteral) propertyName).getValue().equals("rules_file")) {
+        if (isPropertyValue && ((JsonStringLiteral) jsonStringLiteral.getParent().getFirstChild()).getValue().equals("rules_file")) {
+            if (!isRulesFileExist(element, rulesFilePath)) {
+                Annotation badProperty = holder.createErrorAnnotation(element.getTextRange(), "The rules file cannot be found. Correct the path to the rules file");
+                badProperty.setHighlightType(ProblemHighlightType.ERROR);
+            }
+        }
 
-                if (!isRulesFileExist(element, rulesFilePath)) {
-                    // No well-formed property found following the key-separator
-                    Annotation badProperty = holder.createErrorAnnotation(element.getTextRange(), "The rules file cannot be found. Correct the path to the rules file");
-                    //badProperty.setTextAttributes(SimpleSyntaxHighlighter.BAD_CHARACTER);
-                    badProperty.setHighlightType(ProblemHighlightType.ERROR);
-                    // ** Tutorial step 18.3 - Add a quick fix for the string containing possible properties
-                    //badProperty.registerFix(new SimpleCreatePropertyQuickFix(possibleProperties));
+        // If a field name then add it to the hash-map
+        if (isPropertyValue) {
+            PsiElement propertyNameElement = jsonStringLiteral.getParent().getFirstChild();
+            if (propertyNameElement instanceof JsonStringLiteral) {
+                String propertyName = ((JsonStringLiteral) propertyNameElement).getValue();
+
+                if (propertyName.equals("name")) {
+                    PsiElement psiElement1 = element.getParent().getParent().getParent();
+                    PsiElement psiElement2 = psiElement1.getParent();
+                    if (psiElement1 instanceof JsonArray && psiElement2 instanceof JsonProperty &&
+                            psiElement2.getFirstChild() instanceof JsonStringLiteral &&
+                            ((JsonStringLiteral) psiElement2.getFirstChild()).getValue().equals("fields")) {
+                        HashSet<String> fields = filesFieldNames.computeIfAbsent(filePath, k -> new HashSet<>());
+                        fields.add(jsonStringLiteral.getValue());
+                    }
                 }
             }
         }
+    }
+
+    private boolean isPropertyValue(JsonStringLiteral jsonStringLiteral) {
+        if (jsonStringLiteral.getParent() != null) {
+            PsiElement propertyName = jsonStringLiteral.getParent().getFirstChild();
+            if (propertyName != jsonStringLiteral && propertyName instanceof JsonStringLiteral && ((JsonStringLiteral) propertyName).isPropertyName()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private boolean isRulesFileExist(PsiElement element, String rulesFilePath) {
